78eb2ac4750f4f77dda6499832a15304fe8214f7
fannybillefalt <fannybillefalt@gmail.com>
2026-02-04 16:09:53 +0100

    Added UI class, switchmenuclass and dbmanager class. Added a menu structure.

---
+++ VarbergHighschool_FannyBillefält/SwitchMenu.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarbergHighschool_FannyBillefält
+{
+    internal class SwitchMenu
+    {
+        internal void WelcomeMenu()
+        {
+            bool keepRunning = true;
+
+            while (keepRunning)
+            {
+                UI.PrintedWelcomeMenu();
+
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int number))
+                {
+                    Console.WriteLine("Ogiltigt val, försök igen.");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("1. Information om personalen");
+                        //Här ska dbmanager metod ligga för val 1 i printedWelcomeMenu.
+                        break;
+                    case 2:
+                        Console.WriteLine("2. Information om elever");
+                        //Här ska dbmanager metod ligga för val 2 i printedWelcomeMenu.
+                        break;
+                    case 3:
+                        Console.WriteLine("3. Ekonomi");
+                        //Här ska dbmanager metod ligga för val 3 i printedWelcomeMenu.
+                        break;
+                    case 4:
+                        Console.WriteLine("4. Kurser");
+                        //Här ska dbmanager metod ligga för val 4 i printedWelcomeMenu.
+                        break;
+                    case 5:
+                        Console.WriteLine("5. Avsluta");
+                        Console.WriteLine("Hej då! Tack för denna gången.");
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val, försök igen.");
+                        Console.WriteLine("\nTryck Enter för att fortsätta...");
+                        Console.ReadLine();
+                        break;
+                }
+
+
+            }
+        }
+
+        internal void StaffMenu()//gör alla menyer som denna.
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                UI.PrintedWelcomeMenu();
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("1. Antal lärare per avdelning");
+                        break;
+                    case "2":
+                        Console.WriteLine("2. Översikt all personal");
+                        break;
+                    case "3":
+                        Console.WriteLine("3. Addera ny personal");
+                        UI.PrintedAddNewStaff();//Ska ev bort om den ej ska användas. Behöver ju spara med readline, kanske bättre att lägga den som en stöd metod under alla switchmenyer.
+                        break;
+                    case "4":
+                        Console.WriteLine("Tillbaka till huvudmenyn.");//Ska denna ligga här?
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val, försök igen.");
+                        Console.WriteLine("\nTryck Enter för att fortsätta...");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+
+        internal void StudentMenu()
+        {
+            bool keepRunning = true;
+            int number;
+            string input;
+
+            while (!int.TryParse(input = Console.ReadLine(), out number))
+            {
+                //här ska nog vara en using...ELLER?
+
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("1. Översikt elever");
+                        break;
+                    case 2:
+                        Console.WriteLine("2. Översikt betyg");
+                        break;
+                    case 3:
+                        Console.WriteLine("Tillbaka till huvudmenyn.");//Ska denna ligga här?
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val, försök igen.");
+                        Console.WriteLine("\nTryck Enter för att fortsätta...");
+                        Console.ReadLine();
+                        break;
+
+                }
+
+
+            }
+        }
+
+        internal void EconomyMenu()
+        {
+            bool keepRunning = true;
+            int number;
+            string input;
+
+            while (!int.TryParse(input = Console.ReadLine(), out number))
+            {
+
+
+                //här ska nog vara en using...ELLER?
+
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("1. Översikt elever");
+                        break;
+                    case 2:
+                        Console.WriteLine("2. Översikt betyg");
+                        break;
+                    case 3:
+                        Console.WriteLine("Tillbaka till huvudmenyn.");//Ska denna ligga här?
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val, försök igen.");
+                        Console.WriteLine("\nTryck Enter för att fortsätta...");
+                        Console.ReadLine();
+                        break;
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ VarbergHighschool_FannyBillefält/UI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarbergHighschool_FannyBillefält
+{
+    internal class UI
+    {
+        internal static void PrintedWelcomeMenu()
+        {
+            Console.WriteLine("Välkommen till Varberg-Highschool!");
+            Console.WriteLine("__________________________________");
+            Console.WriteLine();
+            Console.WriteLine("1. Information om personalen");
+            Console.WriteLine("2. Information om elever");
+            Console.WriteLine("3. Ekonomi");
+            Console.WriteLine("4. Kurser");
+            Console.WriteLine("5. Avsluta");
+            Console.Write("Ditt val: ");
+        }
+
+        internal static void Printed_StaffMenu()
+        {
+            Console.WriteLine("1. Antal lärare per avdelning");
+            Console.WriteLine("2. Översikt all personal");
+            Console.WriteLine("3. Addera ny personal");
+            Console.Write("Ditt val: ");
+        }
+
+        internal static void Printed_StudentMenu()
+        {
+            Console.WriteLine("1. Översikt elever");
+            Console.WriteLine("2. Översikt betyg");
+            Console.Write("Ditt val: ");
+        }
+
+        internal static void EconomyMenu()
+        {
+            Console.WriteLine("Medellön");
+            Console.WriteLine("Lön per avdelning");
+        }
+
+        internal static void PrintedAddNewStaff()
+        {
+            Console.WriteLine("Fyll i alla information om den nya personalen: ");
+            Console.WriteLine("Förnamn: ");
+            Console.WriteLine("Efternamn: ");
+            Console.WriteLine("Personnummer: ");
+            Console.WriteLine("Mejladress: ");
+            Console.WriteLine("Anställningsdatum: ");
+            Console.WriteLine("Lön: ");
+
+        }
+
+    }
+}
